073bc6c599dcab1fdc4c23fa63a6a4e9b51bf3de
JeffAllison <itsjallison@gmail.com>
2022-06-14 15:56:19 -0700

    added authors by id test in authors.test.js

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -11,7 +11,6 @@ describe('authors routes', () => {
 
   it('/authors should return a list of authors by id and name', async () => {
     const res = await request(app).get('/authors');
-    console.log(res.body, 'body');
     const authors = await Author.getAllAuthors();
     const expected = authors.map((author) => {
       return {
@@ -22,15 +21,15 @@ describe('authors routes', () => {
     expect(res.body).toEqual(expected);
   });
 
-  // it('should return a list of authors with bibliography', async () => {
-  //   const res = await request(app).get('/authors');
-  //   expect(res.body.length).toEqual(9);
-  //   const carl = res.body.find((auth) => auth.id === 1);
-  //   expect(carl).toHaveProperty('id', 1);
-  //   expect(carl).toHaveProperty('full_name', 'Carl Rossi');
-  //   expect(carl).toHaveProperty('date_of_birth', '1922-03-25');
-  //   expect(carl).toHaveProperty('place_of_birth', 'Arlington, Texas');
-  // });
+  it('/authors/:id should return an author from the list', async () => {
+    const res = await request(app).get('/authors/1');
+    const carl = {
+      full_name: 'Carl Rossi',
+      date_of_birth: '1922-03-25',
+      place_of_birth: 'Arlington, Texas',
+    };
+    expect(res.body).toEqual(carl);
+  });
 
   afterAll(() => {
     pool.end();
