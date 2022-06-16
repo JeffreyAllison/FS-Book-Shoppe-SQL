ce757563296efa4f4360c33c009918e9c9b7caf0
JeffAllison <itsjallison@gmail.com>
2022-06-16 15:29:35 -0700

    updated books/:id test to return author information

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -22,12 +22,20 @@ describe('books routes', () => {
     expect(res.body).toEqual(expected);
   });
 
-  it('/books should return a book from the list', async () => {
+  it('/books/:id should return a book from the list with author information', async () => {
     const res = await request(app).get('/books/1');
     const histories = {
       id: '1',
       title: 'A History of Actual Histories',
       released_date: 2000,
+      authors: [
+        {
+          id: '2',
+          full_name: 'Apple Jack',
+          date_of_birth: 1963,
+          place_of_birth: 'Dar es Salaam, Tanzania',
+        },
+      ],
     };
     expect(res.body).toEqual(histories);
   });
